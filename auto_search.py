b84969058dd6b6d9e47b93d811a6edab5af0ddc3
sawadakazuki <1120120u@g.hit-u.ac.jp>
2021-08-10 14:58:25 +0900

    35日バージョン

--- auto_search.py
+++ auto_search.py
@@ -19,7 +19,7 @@ month = today.month
 day = today.day
 print(month,day)
 
-tomorrow = today + datetime.timedelta(days=1)
+tomorrow = today + datetime.timedelta(days=35)
 tomorrow_month =tomorrow.month
 tomorrow_day =tomorrow.day
 print(tomorrow_month,tomorrow_day)
